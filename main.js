b018bca16a92e829c2f010b57826111eb94e8502
ekais <e.kaiser.93@gmail.com>
2017-11-18 16:49:52 -0600

    fixed issues with jquery

--- main.js
+++ main.js
@@ -3,20 +3,25 @@ $(document).ready(function(){
 
 var userSelection = [];
 
-$('.selection-box').on('click', function(){
-  $(this).addClass("clicked");
-  if ($(this).hasClass("clicked")){
-    userSelection = $(this).text();
-    console.log(userSelection);
-    if ($(userSelection).hasClass('.selection-box false')) {
-      $('.selection-box').css('border','#56A589', 'color','#56A589');
-      $('h2').css('color','#56A589');
-      $('h2').text().replace("Correct");
+$('.selection-box').on('click', function(event){
+    $(this).addClass("clicked");
+    if ($(this).hasClass("clicked")){
+      userSelection = $(this);
+      console.log(userSelection);
+        if ($(userSelection).hasClass('false')) {
+          $(userSelection).css('borderColor','#56A589');
+          $(userSelection).css('color','#56A589');
+          $('h2').css('color','#56A589');
+          $('h2').css('textAlign', 'center');
+          $('h2').text("Correct");
+        }
+        else {
+          $(userSelection).css('borderColor', '#DB6F6F');
+          $(userSelection).css('color','#DB6F6F');
+          $('h2').css('color', '#DB6F6F');
+          $('h2').css('textAlign', 'center');
+          $('h2').text("Wrong, Try Again");
+        }
     }
-    else {
-      $(this).css('border', '#DB6F6F', 'color','#DB6F6F');
-      $('h2').css('color', '#DB6F6F');
-      $('h2').text().replace("Wrong, Try Again");
-    }
-  }
+  });
 });
